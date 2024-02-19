927191d2d86f7e38a8ea80ac48e8f4a53edc5d6e
wolvesdevs_workPC <75551104+wolvesdevs@users.noreply.github.com>
2024-02-19 18:00:44 +0900

    refs #004 string配列_クエリ構文（複数条件）

--- src/Lambda2/Lambda2.UI/Views/MainView.Designer.cs
+++ src/Lambda2/Lambda2.UI/Views/MainView.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             button1 = new Button();
+            button2 = new Button();
             SuspendLayout();
             // 
             // button1
@@ -41,11 +42,22 @@
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
             // 
+            // button2
+            // 
+            button2.Location = new Point(29, 63);
+            button2.Name = "button2";
+            button2.Size = new Size(112, 34);
+            button2.TabIndex = 1;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
             // MainView
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1080, 481);
+            Controls.Add(button2);
             Controls.Add(button1);
             Name = "MainView";
             Text = "Form1";
@@ -55,5 +67,6 @@
         #endregion
 
         private Button button1;
+        private Button button2;
     }
 }
--- src/Lambda2/Lambda2.UI/Views/MainView.cs
+++ src/Lambda2/Lambda2.UI/Views/MainView.cs
@@ -34,5 +34,38 @@ namespace Lambda2.UI
                           select num;
             Debug.WriteLine(string.Join(", ", result3));
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // 配列要素は6個。中身は"A"～"FFFFFF"まで。文字数は1文字ずつ増やしていく。例→"A", "BB", "CCC", "DDDD", "EEEEE", "FFFFFF"
+            string[] values = { "A", "BB", "CCC", "DDDD", "EEEEE", "aBC" };
+
+            // すべて取得。クエリ構文で。
+            var result1 = from value in values
+                          select value;
+            Debug.WriteLine($"result1: {string.Join(", ", result1)}");
+
+            var result2 = from value in values
+                          where value.Contains("a")
+                          select value;
+            Debug.WriteLine($"result2: {string.Join(", ", result2)}");
+
+            var result3 = from value in values
+                          where value.ToLower().Contains("a")
+                          select value;
+            Debug.WriteLine($"result3: {string.Join(", ", result3)}");
+
+            var result4 = from value in values
+                          where value.ToLower().Contains("a")
+                          && value.Length >= 3
+                          select value;
+            Debug.WriteLine($"result4: {string.Join(", ", result4)}");
+
+            var result5 = from value in values
+                          where value.ToLower().Contains("a")
+                          || value.Length >= 3
+                          select value;
+            Debug.WriteLine($"result5: {string.Join(", ", result5)}");
+        }
     }
 }
